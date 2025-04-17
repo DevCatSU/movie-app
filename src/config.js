c815b9a932a8e40c5a51575af526f3e3300d0ca4
Kyungsu Kim <roomformilk683@gmail.com>
2025-04-17 08:01:15 +0100

    rollback css

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_KEY = 'fed0305e690481383c6ef53ad4a279bf'; // <-- PASTE YOUR API KEY HERE
+export const API_KEY = 'fed0305e690481383c6ef53ad4a279bf'; 
 export const API_BASE_URL = 'https://api.themoviedb.org/3';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w300';
